18670c3e6d238ac50ae9127c599671b309d3a18c
Connor Turland <connorturland@gmail.com>
2016-03-27 00:01:49 -0700

    https wasn't working

--- processTopics.js
+++ processTopics.js
@@ -4,7 +4,7 @@ function processTopics(data) {
   topics.push({
     name: data.group.name,
     desc: data.group.currency_code + ' ' + data.group.currency_symbol,
-    link: 'https://beta.cobudget.co/#/groups/' + data.group.id,
+    link: 'http://beta.cobudget.co/#/groups/' + data.group.id,
     metacode_id: 638205575, // group
     permission: 'private'
   });
@@ -13,7 +13,7 @@ function processTopics(data) {
     topics.push({
       name: bucket.name,
       desc: bucket.description,
-      link: 'https://beta.cobudget.co/#/buckets/' + bucket.id,
+      link: 'http://beta.cobudget.co/#/buckets/' + bucket.id,
       metacode_id: 991788158, // idea
       permission: 'private'
     });
@@ -23,7 +23,7 @@ function processTopics(data) {
     topics.push({
       name: user.name,
       desc: '',
-      link: 'https://beta.cobudget.co/#/users/' + user.id,
+      link: 'http://beta.cobudget.co/#/users/' + user.id,
       metacode_id: 125146708, // person
       permission: 'private'
     });
