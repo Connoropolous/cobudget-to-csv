03b36c3ef473a561569ed32d7cd24cef226ee630
Connor Turland <connorturland@gmail.com>
2016-04-11 10:59:42 -0400

    save to csv

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# cobudget-metamaps
+# Save Cobudget Bucket Data to a CSV file
+
+## Instructions
+
+1. clone the code
+2. create a file in the code folder called `.env`
+3. add the following into the `.env` file
+
+```
+EMAIL=yourcobudgetemail
+PASSWORD=yourcobudgetpassword
+GROUP_ID=yourcodbudgetgroupid
+```
+
+4. execute `node index.js` from the command line
+5. look in your folder for a file named `cobudgetBuckets-DATE.csv`
+6. Rejoice
--- index.js
+++ index.js
@@ -1,66 +1,38 @@
 require('dotenv').config();
-var token = process.env.METAMAPS_TOKEN;
+var fs = require('fs');
 var async = require('async');
-var mm = require('metamaps-client');
 var CobudgetClient = require('cobudget-client');
 var myClient = new CobudgetClient({ email: process.env.EMAIL, password: process.env.PASSWORD });
-
-var map = {
-  name: 'cobudget model map',
-  desc: 'directly from your friendly neighbourhood cobudget',
-  permission: 'private',
-  arranged: true
-};
-
-var randomCoord = function () {
-  var min = -1500, max = 1500;
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+var json2csv = require('json2csv');
 
 myClient.init(function () {
   myClient.getItAll(function (err, results) {
     if (err || !results) return console.log('didn\'t work');
-    async.waterfall([
-      // create map
-      async.apply(mm.createMap, map, token),
-      // create topics
-      function (map, callback) {
-        var topicsToCreate = require('./processTopics.js')(results);
-        var numCreated = 1;
-        async.mapLimit(
-           topicsToCreate,
-           5, // max number of calls to make at once
-           function (topic, cb) {
-             mm.addTopicToMap(token, map.id, randomCoord(), randomCoord(), topic, cb);
-             console.log(numCreated + '/' + topicsToCreate.length + ' topics');
-             numCreated++;
-           },
-           function (err, topics) {
-             if (err) return callback(err);
-             callback(null, topics, map);
-           });
-      },
-      // create synapses
-      function (topics, map, callback) {
-        console.log('created topics');
-        var synapsesToCreate = require('./processSynapses.js')(results, topics);
-        var numCreated = 1;
-        async.mapLimit(
-           synapsesToCreate,
-           5,
-           function (synapse, cb) {
-             mm.addSynapseToMap(token, map.id, synapse, cb);
-             console.log(numCreated + '/' + synapsesToCreate.length + ' synapses');
-             numCreated++;
-           },
-           function (err, synapses) {
-             if (err) return callback(err);
-             callback(null, topics, synapses, map);
-           });
-      }
-    ], function (err, topics, synapses, map) {
-      if (err) return console.log('everything broke: ' + err);
-      console.log('created EVERYTHING');
+
+    var data = [];
+    results.buckets.forEach(function (b) {
+      data.push({
+        name: b.name,
+        creator: b.author_name,
+        funded: b.status === 'funded',
+        funded_at: b.funded_at,
+        target: b.target,
+        total_contributions: b.total_contributions,
+        description: b.description,
+        url: 'http://beta.cobudget.co/#/buckets/' + b.id
+      });
+    });
+
+    var fields = [
+      'name', 'creator', 'funded', 'funded_at', 'target', 'total_contributions', 'description', 'url'
+    ];
+    json2csv({ data: data, fields: fields }, function(err, csv) {
+      if (err) console.log(err);
+      
+      fs.writeFile('cobudgetBuckets-' + Date.now() + '.csv', csv, function(err) {
+        if (err) console.log(err);
+        else console.log('file saved');
+      });
     });
   });
 });
